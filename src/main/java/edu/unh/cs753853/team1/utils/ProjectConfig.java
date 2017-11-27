6f45be03dc1b5a8285feff02313eeaa6e4577166
amf2015 <amf2015@wildcats.unh.edu>
2017-11-27 12:25:07 -0500

    Fixed directory
    Added a / to the end of the directory name to make sure future directorys append correctly.

--- src/main/java/edu/unh/cs753853/team1/utils/ProjectConfig.java
+++ src/main/java/edu/unh/cs753853/team1/utils/ProjectConfig.java
@@ -4,7 +4,7 @@ public class ProjectConfig {
 	public static String TEAM_NAME = "Team 1";
 	public static String INDEX_DIRECTORY = "index";
 	public static String OUTPUT_DIRECTORY = "output";
-	public static String STACK_DIRECTORY = "stackoverflow";
+	public static String STACK_DIRECTORY = "stackoverflow/";
 	public static String CS_STACK_DIRECTORY = "stackoverflow/cs_stackoverflow/";
 
 	// Method
